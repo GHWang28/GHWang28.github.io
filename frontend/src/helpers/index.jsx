d0dfd576e31637a7f57ce8a4bf36f12556eb39b8
Gordon Wang <97267381+GHWang28@users.noreply.github.com>
2022-11-29 11:04:33 +1100

    Added docstring to rng

--- frontend/src/helpers/index.jsx
+++ frontend/src/helpers/index.jsx
@@ -1,6 +1,12 @@
 import getVideoId from 'get-video-id';
 import config from '../config.json'
 
+/**
+ * Generates a random integer between min and max inclusively
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Number}
+ */
 export function rng(min, max) {
   return Math.round(Math.random() * (max - min)) + min
 }
