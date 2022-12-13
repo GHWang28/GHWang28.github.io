f5c1c8526892ff01b4fb081c89d97a707f58be6d
Gordon <z5309206@ad.unsw.edu.au>
2022-12-13 14:45:58 +1100

    Made scroll to top have smooth behavior

--- frontend/src/wrappers/ScrollToTop/index.jsx
+++ frontend/src/wrappers/ScrollToTop/index.jsx
@@ -5,7 +5,7 @@ export default function ScrollToTop({ children }) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({top: 0, behavior: 'smooth'});
   }, [pathname]);
 
   return children;
