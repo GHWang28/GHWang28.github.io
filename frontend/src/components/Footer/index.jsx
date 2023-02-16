585062392c729bb1eb4e07c2bd0b694199b73ccd
Gordon <z5309206@ad.unsw.edu.au>
2023-02-16 23:06:21 +1100

    Added Footer

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,23 +1,56 @@
-import { Box } from '@mui/material';
 import React from 'react';
+import { Box, Typography, useTheme } from '@mui/material';
+import GitInfo from 'react-git-info/macro';
 import { useLocation } from 'react-router';
+import { ISOToDateStr } from '../../helpers';
+import { useDispatch } from 'react-redux';
+import { setImageZoom } from '../../redux/actions';
 
 export default function Footer () {
   const location = useLocation();
+  const gitInfo = GitInfo();
+  const lightMode = (useTheme().palette.mode === 'light');
+  const dispatch = useDispatch();
+  const logoSrc = (lightMode) ? '/images/gw-logo.png' : '/images/gw-logo-light.png';
+  const onLogoClick = () => {
+    dispatch(setImageZoom(logoSrc))
+  }
 
   if (location.pathname === '/') return null;
 
   return (
     <Box
       component='footer'
-      mt={5}
       sx={{
-        height: '100px',
+        height: '120px',
         width: '100vw',
-        boxShadow: 'inset 0 50px 50px -6px rgba(0,0,0,0.5)'
+        boxShadow: `inset 0 50px 50px -6px rgba(0,0,0,${(lightMode) ? '0.15' : '0.5'})`
       }}
     >
       <Box component='hr' sx={{ width: '95%' }} />
+      <Box sx={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 1 }}>
+        <Box
+          component='img'
+          src={logoSrc}
+          onClick={onLogoClick}
+          sx={{
+            height: '30%',
+            '&:hover': {
+              scale: '1.2'
+            },
+            cursor: 'pointer',
+            transition: 'scale 0.2s ease-in-out'
+          }}
+        />
+        <Box sx={{ height: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <Typography fontFamily='my-handwriting' sx={{ lineHeight: 1, fontWeight: 'bold' }}>
+            {'Designed by Gordon Wang 2023 ©'}
+          </Typography>
+          <Typography fontFamily='my-handwriting' sx={{ lineHeight: 1 }}>
+            {`Last updated: ${ISOToDateStr(gitInfo.commit.date)}`}
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   )
 }
