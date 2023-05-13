1faf6f48c4f7a08f4da590bc2b549b7dc4640b3a
Gordon Wang <gordon.wang280801@gmail.com>
2023-05-13 15:04:46 +1000

    Updated hobbies description

--- frontend/src/pages/PageAbout/HobbiesSection/hobbies.js
+++ frontend/src/pages/PageAbout/HobbiesSection/hobbies.js
@@ -15,15 +15,15 @@ const generateHobbies = () => {
 
   return [
     {
-      text: 'As a child, I was always drawn to projects that were both visually stunning and intellectually stimulating. This passion has continued to drive me in my pursuit of knowledge and skill in the mediums I use to create these engaging visual experiences, be it through the medium of digital art or the precision of programming.',
-      icon: <PaletteIcon {...props} />
-    },
-    {
-      text: 'I am also incredibly passionate about game design. There is a unique thrill in creating an interactive medium that others can engage with and it is incredibly satisfying to see them interact with something I have designed. Incorporating user\'s feedback to further improve the game\'s design adds to the satisfaction as it further fuels my creative thinking. This passion drives me to constantly seek out new and innovative ways to create engaging and interesting experiences for players.',
+      text: 'I am incredibly interested in game design. I find a unique thrill and satisfaction in creating an interactive medium that others can engage with and explore. Incorporating user\'s feedback to further improve the game\'s design adds to the satisfaction as it further fuels my creative thinking. This passion drives me to constantly seek out new and innovative ways to create engaging and interesting experiences for players. Currently I am learning how to program with Unity and Godot to create an indie game.',
       icon: <SportsEsportsIcon {...props} />
     },
     {
-      text: 'I have a passion for teaching. It is deeply satisfying to watch others learn and grow, and I take great pleasure in providing guidance and support to help them achieve their goals. Currently, I offer private tutoring services to friends and family members. In addition, I am also a tutor for Frontend Programming at UNSW.',
+      text: 'I have always been drawn to projects that are visually stunning. This passion has continued to drive me in my pursuit of knowledge and skill in the mediums I use to create these engaging visual experiences, be it through the medium of digital art or the precision of programming.',
+      icon: <PaletteIcon {...props} />
+    },
+    {
+      text: 'I have a passion for teaching as it is deeply satisfying to watch others learn and grow. I take great pleasure in providing guidance and support to help them achieve their goals. Currently, I offer private tutoring services to friends and family members and am also a tutor for Frontend Programming at UNSW.',
       icon: <SchoolIcon {...props} />
     }
   ]
