71212bee18bfad6a42da4fb1f9439a475774586e
Gordon Wang <gordon.wang280801@gmail.com>
2023-08-22 13:16:58 +1000

    Reintroduced Million.js

--- ghwang28/vite.config.ts
+++ ghwang28/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import million from 'million/compiler';
+import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react(), million.vite({ auto: true })],
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   build: {
     sourcemap: false,
   },
