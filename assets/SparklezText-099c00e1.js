1670e5c5e064072f47a45d265bfff70b5d111f3c
Gordon Wang <gordon.wang280801@gmail.com>
2023-09-04 13:09:45 +1000

    Updates

---
+++ assets/SparklezText-099c00e1.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-129bc262.js";import{S as r}from"./index-ba8dff8a.js";const m=({children:a,gradient:t="",noSparklez:e=!1})=>e?s.jsx("span",{className:`gradient-text ${t}`,children:a}):s.jsx(r,{children:s.jsx("span",{className:`gradient-text ${t}`,children:a})});export{m as S};
